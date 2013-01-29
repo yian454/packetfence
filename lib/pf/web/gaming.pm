9fd6698740150af051c8e9432f14f58c324fcfd4
James Rouzier <jrouzier@inverse.ca>
2013-01-29 06:53:16 -0500

    Refactored duplicated code push responsibility to the script Refactored to use function in pf::web::generate_generic_page in of render_template directly Load OUI and gaming console type from files Documented functions

--- lib/pf/web/gaming.pm
+++ lib/pf/web/gaming.pm
@@ -23,37 +23,13 @@ use pf::enforcement qw(reevaluate_access);
 use pf::node qw(node_register is_max_reg_nodes_reached);
 use pf::util;
 use pf::web;
+use pf::web::custom;    # called last to allow redefinitions
 
 Readonly our $GAMING_LOGIN_TEMPLATE   => 'gaming-login.html';
 Readonly our $GAMING_LANDING_TEMPLATE => 'gaming-landing.html';
 Readonly our $GAMING_REGISTRATION_TEMPLATE => 'gaming-registration.html';
-Readonly our @GAMING_OUI => (
-    '00:12:5A:' ,  #          Microsoft-Xbox
-    '00:0D:3A:' ,  #          Microsoft-Xbox
-    '00:50:F2:' ,  #          Microsoft-Xbox
-    '00:01:4A:' ,  #          Sony-PS2-PSP
-    '00:02:C7:' ,  #          Sony-PS2-PSP
-    '00:04:1F:' ,  #          Sony-PS2-PSP
-    '00:13:15:' ,  #          Sony-PS2-PSP
-    '00:09:BF:' ,  #          Nintendo-Wii
-    '00:17:AB:' ,  #          Nintendo-Wii
-    '00:17:FA:' ,  #          Microsoft-Xbox
-    '00:15:C1:' ,  #          Sony-PS3
-    '00:19:C5:' ,  #          Sony-PS3
-    '00:1D:D8:' ,  #          Microsoft-Xbox
-    '00:0B:E6:' ,  #          Nintendo-Wii
-    '00:16:56:' ,  #          Nintendo-Wii
-    '00:1A:E9:' ,  #          Nintendo-Wii
-    '00:1D:0D:' ,  #          Sony-PS3
-    '00:19:1D:' ,  #          Nintendo-Wii
-    '00:19:FD:' ,  #          Nintendo-Wii
-    '00:1F:32:' ,  #          Nintendo-Wii
-    '00:1C:BE:' ,  #          Nintendo-Wii
-    '00:1B:EA:' ,  #          Nintendo-Wii
-    '00:1E:35:' ,  #          Nintendo-Wii
-    '00:1B:7A:' ,  #          Ninetndo-Wii
-    '00:22:48:' ,  #          Microsoft-Xbox
-);
+Readonly our @GAMING_OUI => _load_file_into_array($allowed_gaming_oui_file);
+Readonly our @GAMING_CONSOLE_TYPES => _load_file_into_array($allowed_gaming_console_types_file);
 
 =head1 SUBROUTINES
 
@@ -62,19 +38,41 @@ Readonly our @GAMING_OUI => (
 =cut
 
 
-sub authenticate {
-    my ($portalSession,$cgi, $session, $info,$logger) = @_;
-    my ($auth_return,$err) = pf::web::web_user_authenticate($portalSession, $cgi->param("auth"));
-    if ($auth_return == 1) {
-        $info->{'category'} = $portalSession->getProfile->getGuestCategory;
-        $session->param(login => $cgi->param('username'));
+=item _load_file_into_array
+
+Loads each line of file into array
+Trimming spaces and removing shell style comments from each line
+
+=cut
+
+sub _load_file_into_array {
+    my ($file_name) = @_;
+    my @items;
+    if(-r $file_name) {
+        local *FILE;
+        open(FILE,$file_name);
+        @items =
+            grep {$_}
+            map {
+                #Getting rid of newlines, comments and trimming spaces
+                chomp; s/#.*$//; s/^\s+//; s/\s+$//;
+                $_;
+            } <FILE>;
+        close(FILE);
     }
-    return ( $auth_return, $err );
+    return @items;
 }
+
+=item generate_login_page
+
+Generates the login page
+
+=cut
 
 sub generate_login_page {
     my ( $portalSession, $err ) = @_;
-    _generate_page($portalSession,$GAMING_LOGIN_TEMPLATE,
+    pf::web::generate_generic_page($portalSession,$GAMING_LOGIN_TEMPLATE,
+        {
         txt_auth_error => ( (defined $err) ? i18n($err) : undef)  ,
         username => encode_entities($portalSession->cgi->param("username")),
         selected_auth => ( encode_entities($portalSession->cgi->param("auth")) || $portalSession->getProfile->getDefaultAuth),
@@ -82,67 +80,64 @@ sub generate_login_page {
         oauth2_google => $guest_self_registration{$SELFREG_MODE_GOOGLE},
         oauth2_facebook => $guest_self_registration{$SELFREG_MODE_FACEBOOK},
         oauth2_github => $guest_self_registration{$SELFREG_MODE_GITHUB},
+        }
     );
 }
 
-sub generate_landing_page {
-    my ($portalSession, $msg) = @_;
-    _generate_page($portalSession,$GAMING_LANDING_TEMPLATE, status_msg => ( (defined $msg ) ? i18n($msg) : undef));
-}
+=item generate_registration_page
+
+Generate registration page
+
+=cut
 
 sub generate_registration_page {
     my ($portalSession, $err) = @_;
-    _generate_page($portalSession,$GAMING_REGISTRATION_TEMPLATE, txt_auth_error => ( (defined $err) ? i18n($err) : undef));
+    pf::web::generate_generic_page(
+        $portalSession,
+        $GAMING_REGISTRATION_TEMPLATE, {
+            console_types => \@GAMING_CONSOLE_TYPES,
+            txt_auth_error => ( (defined $err) ? i18n($err) : undef),
+        }
+    );
 }
 
-sub _generate_page {
-    my ($portalSession, $template,%args) = @_;
-    my @keys = keys %args;
-    @{$portalSession->stash}{@keys} = @args{@keys};
-    render_template($portalSession, $template);
+
+=item generate_landing_page
+
+Generates the landing page
+
+=cut
+
+sub generate_landing_page {
+    my ($portalSession, $msg) = @_;
+    pf::web::generate_generic_page(
+        $portalSession,
+        $GAMING_LANDING_TEMPLATE,
+        {status_msg => ( (defined $msg) ? i18n($msg) : undef)}
+    );
 }
 
-sub register_node {
-    my ( $portalSession, $pid, $mac, %info ) = @_;
-    my $logger = Log::Log4perl::get_logger(__PACKAGE__);
-    my ($result,$msg);
-    if(!valid_mac($mac) || !is_gaming_mac($mac)) {
-        $msg = "Please verify MAC address provided";
-    }
-    elsif ( is_max_reg_nodes_reached($mac, $pid, $info{'category'}) ) {
-        $msg = "You have reached the maximum number of devices you are able to register with this username.";
-    }
-    else {
-        ($result,$msg) = _sanitize_and_register($portalSession->session, $mac, $pid, %info);
-    }
-    return ($result,$msg);
-}
 
-sub is_gaming_mac {
+=item is_allowed_gaming_mac
+
+Validate if mac address is an allowed gaming mac
+
+=cut
+
+sub is_allowed_gaming_mac {
     my ($mac) = @_;
-    foreach my $oui (@GAMING_OUI) {
-        return 1 if $mac =~ /^\Q$oui\E/i;
+    my $result = 0;
+    if(@GAMING_OUI) {
+        if($mac) {
+            foreach my $oui (@GAMING_OUI) {
+                last if ($result = $mac =~ /^\Q$oui\E/i);
+            }
+        }
+    } else {
+        $result = 1;
     }
-    return 0;
+    return $result;
 }
-
-sub _sanitize_and_register {
-    my ( $session, $mac, $pid, %info ) = @_;
-    my $logger = Log::Log4perl::get_logger(__PACKAGE__);
-    my ($result,$msg);
-    if(valid_mac($mac)) {
-        $logger->info("performing node registration MAC: $mac pid: $pid");
-        node_register( $mac, $pid, %info );
-        $result = $TRUE;
-        $msg = "The MAC address %s has been successfully registered.";
-    }
-    else {
-        $msg = "The MAC address %s provided is invalid please try again";
-    }
-    $msg = i18n_format($msg,$mac);
-    return ($result,$msg);
-}
-
 
 =back
 

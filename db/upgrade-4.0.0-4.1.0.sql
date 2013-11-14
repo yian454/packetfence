4cfd0aa8476386aa1b4ef14ee1a55a02f0d12a60
Francis Lachapelle <flachapelle@inverse.ca>
2013-11-14 14:45:38 -0500

    Upgrade db schema for new timeleft column of node

--- db/upgrade-4.0.0-4.1.0.sql
+++ db/upgrade-4.0.0-4.1.0.sql
@@ -3,3 +3,8 @@
 --
 
 ALTER TABLE `temporary_password` MODIFY category int DEFAULT NULL;
+
+--
+-- Add a column to store the remaining available network access time of a node
+--
+ALTER TABLE node ADD `timeleft` int unsigned AFTER `lastskip`;

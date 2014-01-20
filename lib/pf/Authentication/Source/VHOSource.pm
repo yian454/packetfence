614409b6520e8ff5efe5416ad7432312c6aecf5e
James Rouzier <jrouzier@inverse.ca>
2014-01-20 17:27:47 -0500

    The new VHO authentication source

--- lib/pf/Authentication/Source/VHOSource.pm
+++ lib/pf/Authentication/Source/VHOSource.pm
@@ -2,7 +2,7 @@ package pf::Authentication::Source::VHOSource;
 
 =head1 NAME
 
-pf::Authentication::Source::RADIUSSource
+pf::Authentication::Source::VHOSource
 
 =head1 DESCRIPTION
 
@@ -17,6 +17,96 @@ use Moose;
 extends 'pf::Authentication::Source::RADIUSSource';
 
 has '+type' => ( default => 'VHO' );
+
+sub mlogit {
+    my $message = shift;
+    open OUT, ">>/tmp/matt.log";
+    print OUT scalar localtime time;
+    print OUT " ";
+    print OUT $message, "\n";
+    close OUT;
+}
+
+=head2  authenticate
+
+=cut
+
+sub authenticate {
+
+    my ( $self, $username, $password ) = @_;
+
+    my $logger = Log::Log4perl->get_logger('pf::authentication');
+
+    # internet hack since the radius rules can't be changed, try every realm if not defined.
+    # matt joubert
+
+    unless ( $username =~ /\@.*mts.net$/ ) {
+        my @realms = ( 'resa.mts.net', 'res1.mts.net', 'resc.mts.net', 'resd.mts.net', 'rese.mts.net', 'bhs.mts.net', 'bhs1.mts.net', 'bhs2.mts.net', 'bhs3.mts.net', 'bhsp.mts.net', 'bslv1.mts.net', 'bslv3.mts.net', 'bslv5.mts.net', 'bgld1.mts.net', 'bgld3.mts.net', 'bgld5.mts.net', 'wifi.mts.net' );
+
+        my $radius = new Authen::Radius(
+            Host   => "$self->{'host'}:$self->{'port'}",
+            Secret => $self->{'secret'},
+        );
+
+        if ( defined $radius ) {
+            foreach (@realms) {
+                my $username = "$username\@$_";
+                my $result = $radius->check_pwd( $username, $password );
+
+                if ( $radius->get_error() eq 'ENONE' ) {
+
+                    if ($result) {
+                        $logger->error("Success RADIUS Sneaky Auth on realm $_");
+                        return ( $TRUE, 'Successful authentication using RADIUS.' );
+                    }
+                }
+            }
+        }
+
+        # mts my accout screen scraping hack
+        # matt joubert
+        my $ua = LWP::UserAgent->new;
+        $ua->cookie_jar( { file => "$ENV{HOME}/.cookies.txt" } );
+        $ua->agent("Wi-Fi Authenticator/1.0");
+        push @{ $ua->requests_redirectable }, 'POST';
+        $ua->env_proxy;
+        $ua->get('https://www.mts.ca/mts/myaccount');
+        my $post = "action=login&userid=$username&password=$password&rememberme=on&as_fid=m7znkirWJb5WpzJxaT";
+        my $req = HTTP::Request->new( POST => 'https://www2.mts.ca/MTSCASSO/sso' );
+        $req->content_type('application/x-www-form-urlencoded');
+        $req->content($post);
+
+        my $res = $ua->request($req);
+
+        # Check the outcome of the response
+        if ( $res->is_success ) {
+            my $res      = $ua->get("https://www2.mts.ca/mts/myaccount/account+overview");
+            my $buffer   = $res->content;
+            my $internet = 0;
+            my $wireless = 0;
+            my $tv       = 0;
+
+            if ( $buffer =~ /Internet Overview/ ) {
+                $internet = 1;
+            }
+
+            if ( $buffer =~ /Wireless Overview/ ) {
+                $wireless = 1;
+            }
+
+            if ( $buffer =~ /(?<!Change My TV Channels and )Access MyPVR/ ) {
+                $tv = 1;
+            }
+
+            return ( $TRUE, 'Successful authentication using Internet internet.' ) if $internet == 1;
+            return ( $TRUE, 'Successful authentication using Wireless internet.' ) if $wireless == 1;
+            return ( $TRUE, 'Successful authentication using TV.' )                if $tv == 1;
+        }
+
+        $logger->error( "Unable to perform RADIUS authentication on any server: " . Authen::Radius::get_error() );
+        return ( $FALSE, 'Unable to authenticate successfully using RADIUS.' );
+    }
+}
 
 =head1 AUTHOR
 

79d62d8e0d17055aab4933802dcd095e7c098718
Louis Munro <lmunro@inverse.ca>
2014-05-22 13:55:49 -0400

    Added a wee bit of logging.

--- lib/pf/api.pm
+++ lib/pf/api.pm
@@ -134,6 +134,7 @@ sub desAssociate {
 
     my ($switchdeauthMethod, $deauthTechniques) = $switch->deauthTechniques($switch->{'_deauthMethod'},$postdata{'connection_type'});
 
+    $logger->info("DeAssociating mac $postdata{'mac'} on switch " . $switch->{_id});
     $deauthTechniques->($switch,$postdata{'mac'});
 }
 

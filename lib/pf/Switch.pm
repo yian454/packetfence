c4ca1a628e41dd02b9f283272a543a38b5682590
James Rouzier <jrouzier@inverse.ca>
2014-05-31 14:43:22 -0400

    remove handleTrap method

--- lib/pf/Switch.pm
+++ lib/pf/Switch.pm
@@ -2878,19 +2878,6 @@ sub extractVLAN {
     return;
 }
 
-=item handleTrap 
-
-=cut
-
-sub handleTrap {
-    my ($this,$trapInfo,$oids) = @_;
-    my $logger = get_logger();
-    $logger->debug( "trap ignored, not useful for switch");
-}
-
-
-
-
 =item parseReceivedFrom
 
 =cut

f4a758b047a8a1c04862b0f07ad4193a8406faa7
Durand Fabrice <fdurand@inverse.ca>
2014-12-22 16:57:14 -0500

    Use Scan config per portal profile

--- lib/pf/Portal/Profile.pm
+++ lib/pf/Portal/Profile.pm
@@ -25,6 +25,7 @@ use pf::log;
 use pf::node;
 use pf::factory::provisioner;
 use pf::os;
+use pf::ConfigStore::Scan;
 
 =head1 METHODS
 
@@ -402,6 +403,60 @@ sub findProvisioner {
         return;
     }
     return first { $_->match($os,$node_attributes) } $self->provisionerObjects;
+}
+
+=item getScans
+
+Returns the Scans IDs for the profile
+
+=cut
+
+sub getScans {
+    my ($self) = @_;
+    return $self->{'_scans'};
+}
+
+=item scanObjects
+
+The scanObjects
+
+=cut
+
+sub scanObjects {
+    my ($self) = @_;
+    return grep { defined $_ } map { pf::factory::scan->new($_) } @{ $self->getScans || [] };
+}
+
+=item findScan
+
+return the first scan that match the device
+
+=cut
+
+sub findScan {
+    my ($self, $mac, $node_attributes) = @_;
+    my $logger = get_logger();
+    my $scancs = pf::ConfigStore::Scan->new();
+    $node_attributes ||= node_attributes($mac);
+    my ($fingerprint) =
+      dhcp_fingerprint_view( $node_attributes->{'dhcp_fingerprint'} );
+    foreach my $scan (split(',',$self->getScans)) {
+        my $scan_config = $scancs->read($scan);
+        if (!defined($scan_config->{'oses'}) && !defined($scan_config->{'categories'})) {
+            return $scan_config;
+        } elsif (defined($scan_config->{'oses'}) && defined($scan_config->{'categories'})) {
+            if (grep { $_ eq $fingerprint->{'os'} } @{ $scan_config->{'oses'} } || grep { $_ eq $node_attributes->{'category'} } @{ $scan_config->{'categories'} } ) {
+                return $scan_config;
+            }
+        } elsif (defined($scan_config->{'oses'}) xor defined($scan_config->{'categories'})) {
+            if (defined($scan_config->{'oses'}) && grep { $_ eq $fingerprint->{'os'} } @{ $scan_config->{'oses'} }) {
+                return $scan_config;
+            } elsif (defined($scan_config->{'categories'}) && grep { $_ eq $node_attributes->{'category'} } @{ $scan_config->{'categories'} } ) {
+                return $scan_config;
+            }
+        }
+    }
+    return 0;
 }
 
 =back

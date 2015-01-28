0a96de18984c495de19309282a37538c3e43e226
Derek Wuelfrath <dwuelfrath@inverse.ca>
2015-01-28 10:30:49 -0500

    New authentication source hash with specific admin rights rules

--- html/pfappserver/lib/pfappserver/Authentication/Store/PacketFence/User.pm
+++ html/pfappserver/lib/pfappserver/Authentication/Store/PacketFence/User.pm
@@ -41,7 +41,7 @@ sub supported_features {
 sub check_password {
   my ($self, $password) = @_;
 
-  my $internal_sources = pf::authentication::getInternalAuthenticationSources();
+  my $internal_sources = pf::authentication::getAdminInternalAuthenticationSources();
   my ($result, $message, $source_id) = &pf::authentication::authenticate($self->_user, $password, @{$internal_sources});
 
   if ($result) {

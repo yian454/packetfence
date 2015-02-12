4f99793348961133b5d2f1a4a79fcf1cb75ca20c
Derek Wuelfrath <dwuelfrath@inverse.ca>
2015-02-11 23:25:03 -0500

    Post rebase

--- db/upgrade-X.X.X-X.Y.Z.sql
+++ db/upgrade-X.X.X-X.Y.Z.sql
@@ -1,3 +1,10 @@
 ---
 --- PacketFence SQL schema upgrade from X.X.X to X.Y.Z
 ---
+
+--
+-- Insert a new 'default' user
+--
+
+INSERT INTO `person` (pid,notes) VALUES ("default","Default User - do not delete");
+INSERT INTO temporary_password (pid, password, valid_from, expiration, access_duration, access_level, category) VALUES ('default', 'default', NOW(), '2038-01-01', NULL, 'NONE', NULL);
